fcce9fd3aec9bee6d0f35345e3b4776cbd580386
areeshc5 <31863419+areeshc5@users.noreply.github.com>
2018-01-08 09:01:01 -0500

    Update ProgressInterfaceAreej.java

--- OrcMathGUI/src/simon/ProgressInterfaceAreej.java
+++ OrcMathGUI/src/simon/ProgressInterfaceAreej.java
@@ -3,5 +3,8 @@ package simon;
 import guiTeacher.interfaces.Visible;
 
 public interface ProgressInterfaceAreej extends Visible {
-
+    void gameOver();
+	  void setRound(int num);
+	  void setSequenceSize(int num);
+  
 }
